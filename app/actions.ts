e338ab104f9039f1b518d780eb47f7dba7968a8b
hi-lee-mon <shunmel.09@gmail.com>
2025-02-23 19:54:39 +0900

    jwtから認証情報を参照する実装

--- app/actions.ts
+++ app/actions.ts
@@ -1,10 +1,32 @@
 'use server'
 
+import jwt from 'jsonwebtoken'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
+
+const AUTH_SECRET = process.env.AUTH_SECRET!
 
 export async function logout() {
   const cookieStore = cookies()
   cookieStore.delete('token')
   redirect('/auth/login')
+}
+
+type Payload = {
+  id: number
+  email: string
+  iat: number
+  exp: number
+}
+export async function getUser(): Promise<Payload> {
+  const cookieStore = cookies()
+  const token = cookieStore.get('token')
+
+  if (!token) {
+    redirect('/auth/login')
+  }
+
+  const decoded = jwt.verify(token.value, AUTH_SECRET) as Payload // { id: 23, email: 'test@example.com', iat: 1740307739, exp: 1740311339 }
+
+  return decoded
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,34 @@
-import { logout } from '@/app/actions'
+import { getUser, logout } from '@/app/actions'
 import { prisma } from '@/util/prisma'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 export default async function Home() {
-  const users = await prisma.user.findMany()
   const cookieStore = cookies()
   const token = cookieStore.get('token')
-  console.log("cookieStore.get('token')=>", token)
 
   if (!token) {
     redirect('/auth/signup')
   }
 
+  const myProfile = await getUser()
+
+  const users = await prisma.user.findMany()
+
   return (
     <div>
       <h1 className="text-2xl font-bold">ユーザ一覧</h1>
+      <p>{`ユーザID：${myProfile.id} がログインしています`}</p>
+
       <ul>
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li key={user.id}>{user.email}</li>
         ))}
       </ul>
       <form action={logout}>
-        <button type="submit">ログアウト</button>
+        <button className="rounded-md bg-blue-500 p-2 text-white" type="submit">
+          ログアウト
+        </button>
       </form>
     </div>
   )
