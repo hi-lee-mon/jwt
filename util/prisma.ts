9a97c2b76cdc80964411e3e0a88c8a3a22455692
hi-lee-mon <shunmel.09@gmail.com>
2025-02-23 18:03:07 +0900

    prisma init

---
+++ util/prisma.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from '@prisma/client'
+
+/**
+ * 次を参照して実装
+ * https://www.prisma.io/docs/orm/more/help-and-troubleshooting/nextjs-help
+ */
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma = globalForPrisma.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
